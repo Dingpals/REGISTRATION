b891c1191d990f588f3cf115bc73687c23b6b37c
Dingpals <150411456+Dingpals@users.noreply.github.com>
2023-11-21 15:18:58 +0800

    Add files via upload

---
+++ SAGRADO/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"SAGRADOREGISTRATION","l":"REGISTRATION"},{"p":"com.mycompany.sagrado","l":"SAGRADO"}];updateSearchResults();
